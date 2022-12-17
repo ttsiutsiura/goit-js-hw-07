f80cf9c355bb825a17a325371414820f81842370
Kirill Tsiutsiura <kirill.tsiutsiura7@gmail.com>
2022-12-17 18:13:25 +0200

    Второе задание

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,8 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+console.log(galleryItems);
+
 const galleryEl = document.querySelector(".gallery");
 
 const galleryMarkup = createGalleryMarkup();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup();
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, description, original }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a>`;
+    })
+    .join("");
+}
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+const lightBox = new SimpleLightbox(".gallery a");
+
+lightBox.defaultOptions.captionDelay = 250;
